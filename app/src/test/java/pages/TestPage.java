367efa2304f69d52d3fbdd44aea58ab7964fe536
uilianks <uilianks@gmail.com>
2021-09-03 20:20:59 -0300

    Primeiro get feito

--- app/src/test/java/pages/TestPage.java
+++ app/src/test/java/pages/TestPage.java
@@ -1,8 +1,18 @@
 package pages;
 
-public class TestPage {
+import io.restassured.RestAssured;
+import org.hamcrest.Matchers;
+import org.hamcrest.core.Is;
 
+import static io.restassured.RestAssured.when;
+
+public class TestPage{
     public boolean executeGet() {
+        try {
+            when().get("https://reqres.in/api/users?page=2").then().statusCode(200).body("page", Matchers.is(2));
+        }catch (Exception e){
+            return false;
+        }
         return true;
     }
 }
